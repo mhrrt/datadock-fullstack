6e621a43a4e7494ee728db836829b883fc24015f
Mhrrt <manishhtrivedi@gmail.com>
2026-05-29 23:15:18 +0530

    updated cors method in app.ts for datadockback

--- DataDockBack/src/app.ts
+++ DataDockBack/src/app.ts
@@ -14,12 +14,15 @@ const app = express();
 
 // Middleware
 // app.use(cors());
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    process.env.FRONTEND_URL || ""
-  ]
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://datadock-fullstack.vercel.app/",
+    ],
+    credentials: true,
+  }),
+);
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
