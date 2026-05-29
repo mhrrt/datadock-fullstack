687fae2e8631f65c8aaedcfadc64714ae44cf5a5
Mhrrt <manishhtrivedi@gmail.com>
2026-05-29 19:40:54 +0530

    added API_URL for Vercel and Render deployment

--- DataDockBack/src/app.ts
+++ DataDockBack/src/app.ts
@@ -13,7 +13,13 @@ import pincodeRoutes from "./routes/pincodeRoutes";
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    process.env.FRONTEND_URL || ""
+  ]
+}));
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
