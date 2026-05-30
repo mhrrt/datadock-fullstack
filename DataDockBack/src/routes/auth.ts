28c56cd5aed75536dc81d163098b9b4b84102bf6
Mhrrt <manishhtrivedi@gmail.com>
2026-05-30 17:17:43 +0530

    updated backend src/routes/auth.ts to bypass auth logic for now

--- DataDockBack/src/routes/auth.ts
+++ DataDockBack/src/routes/auth.ts
@@ -29,47 +29,58 @@ router.post("/login", async (req, res) => {
     }
 
     const { username, password } = parsed.data;
+    //add logic to temp bypass auth for Render deployment
+    if (username === "admin" && password === "admin123") {
+  return res.json({
+    token: "test-token",
+    user: {
+      id: 1,
+      userName: "admin",
+      fullName: "Admin User",
+    },
+  });
+}
+// uncomment when need to add auth logic to flow or when above code get removed
+    // const user = await prisma.user.findUnique({
+    //   where: {
+    //     username,
+    //   },
+    // });
 
-    const user = await prisma.user.findUnique({
-      where: {
-        username,
-      },
-    });
+    // if (!user) {
+    //   return res.status(401).json({
+    //     message: "Invalid username or password",
+    //   });
+    // }
 
-    if (!user) {
-      return res.status(401).json({
-        message: "Invalid username or password",
-      });
-    }
+    // const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
 
-    const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
+    // if (!isPasswordValid) {
+    //   return res.status(401).json({
+    //     message: "Invalid username or password",
+    //   });
+    // }
 
-    if (!isPasswordValid) {
-      return res.status(401).json({
-        message: "Invalid username or password",
-      });
-    }
-
-    const token = jwt.sign(
-      {
-        userId: user.id,
-        // role: user.role,
-      },
-      process.env.JWT_SECRET! || "datadock-secret",
-      {
-        expiresIn: "7d",
-      },
-    );
-
-    return res.json({
-      token,
-      user: {
-        id: user.id,
-        userName: user.username,
-        // role: user.role, dont have user.role
-        fullName: user.fullName,
-      },
-    });
+    // const token = jwt.sign(
+    //   {
+    //     userId: user.id,
+    //     // role: user.role,
+    //   },
+    //   process.env.JWT_SECRET! || "datadock-secret",
+    //   {
+    //     expiresIn: "7d",
+    //   },
+    // );
+// 
+    // return res.json({
+    //   token,
+    //   user: {
+    //     id: user.id,
+    //     userName: user.username,
+    //     // role: user.role, dont have user.role
+    //     fullName: user.fullName,
+    //   },
+    // });
   } catch (error) {
     console.error(error);
 
